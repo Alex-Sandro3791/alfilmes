721a49cdc4ad37277cf144db006c86e755b86cc0
Alex <arapiraca1973@gmail.com>
2024-06-06 09:27:41 -0300

    adicionei meu site de filmes testes

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+var container = document.getElementById("container");
+var button = document.getElementById("action-btn");
+
+button.addEventListener("click", function() {
+
+	var container = document.getElementById("container");
+
+	if (container.style.display === "none") {
+		container.style.display = "block";
+	} else {
+		container.style.display = "none";
+	}
+
+});
